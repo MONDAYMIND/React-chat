e1e1575e65a29fa599f27d2aea92141be5dd8ab2
Анна Стексова <anya-steksova@mail.ru>
2022-08-28 16:16:38 +1000

    fix the loading text in chat to 118next

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -73,7 +73,7 @@ const Chat = () => {
   return connectionError ? (
     <div className="h-100 d-flex justify-content-center align-items-center">
       <div role="status" className="spinner-border text-primary">
-        <span className="visually-hidden">Загрузка...</span>
+        <span className="visually-hidden">{t('loading')}</span>
       </div>
     </div>
   ) : (
